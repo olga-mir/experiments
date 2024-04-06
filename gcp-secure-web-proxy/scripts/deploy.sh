ceb9c0d32c40fb917c0671ff7d6ccd90119ee163
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2024-04-07 09:24:45 +1000

    restructure deploy script and swp manifests

--- gcp-secure-web-proxy/scripts/deploy.sh
+++ gcp-secure-web-proxy/scripts/deploy.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-set -eoux pipefail
+set -eou pipefail
 
 # https://cloud.google.com/secure-web-proxy/docs/overview
 # https://cloud.google.com/secure-web-proxy/docs/initial-setup-steps
 
-# resource name should start with a letter and can only have lowercase letters, numbers, hyphens and at most 63 characters
-# experiment_name="basic"
-experiment_name="with-sa"
-
 vpc_name=$CLUSTER_VPC # sourced in env vars
 subnet_name=$CLUSTER_SUBNET
 
-gsa_email="test-vm-swp@${PROJECT_ID}.iam.gserviceaccount.com"
+# gsa_email="test-vm-swp@${PROJECT_ID}.iam.gserviceaccount.com"
+# TODO: sa=""
 
-TEMPLATES="../manifests-templates"
-RENDERED="../manifests-rendered"
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+TEMPLATES="$SCRIPT_DIR/../manifests-templates"
+RENDERED="$SCRIPT_DIR/../manifests-rendered"
 
-export experiment_name vpc_name subnet_name gsa_email
+export vpc_name subnet_name sa
 
 for template in "$TEMPLATES"/*; do
   filename=$(basename "$template")
   envsubst < "$template" > "$RENDERED/$filename"
 done
 
-policy_name=${experiment_name}-policy
+# gcloud: resource id must consists of no more than 63 characters: lower case letters, digits and hyphens
 
-policy_yaml=$RENDERED/${experiment_name}-policy.yaml
-rule_yaml=$RENDERED/${experiment_name}-rule.yaml
-gateway_yaml=$RENDERED/${experiment_name}-gateway.yaml
+# 1 Gateway : 1 Policy : N Rules
+policy_name=swp-policy
+cert_name=swp-cert
+
+set -x
 
 main() {
 
@@ -40,6 +40,9 @@ if [ $# -eq 0 ]; then
 fi
 
 case "$1" in
+    enable_apis)
+      enable_apis
+        ;;
     generate_certs)
       generate_certs
         ;;
@@ -53,10 +56,14 @@ case "$1" in
       install_proxy
         ;;
     all)
+      enable_apis
       generate_certs
       install_policies
       install_rules
       install_proxy
+        ;;
+    cleanup)
+      cleanup
         ;;
     -h|--help)
         show_help
@@ -69,55 +76,84 @@ case "$1" in
 esac
 }
 
+# Help function to display available options
+show_help() {
+    echo "Usage: $0 [option]"
+    echo "Most common options:"
+    echo "  enable_apis"
+    echo "  all"
+    echo "  cleanup"
+    echo "Or any other function defined in this file"
+}
+
 
 # ---------------- Certificates
 generate_certs() {
   key_path="$HOME/.swp/key.pem"
   cert_path="$HOME/.swp/cert.pem"
-  cert_name="${experiment_name}-cert"
   SWP_HOST_NAME="myswp.example.com"
   days=30
-  # openssl req -x509 -newkey rsa:2048 \
-  #   -keyout $key_path \
-  #   -out $cert_path -days $days \
-  #   -subj "/CN=$SWP_HOST_NAME" -nodes -addext \
-  #   "subjectAltName=DNS:$SWP_HOST_NAME"
+  openssl req -x509 -newkey rsa:2048 \
+    -keyout $key_path \
+    -out $cert_path -days $days \
+    -subj "/CN=$SWP_HOST_NAME" -nodes -addext \
+    "subjectAltName=DNS:$SWP_HOST_NAME"
 
-  # gcloud certificate-manager certificates create $cert_name \
-  #    --certificate-file=$cert_path \
-  #    --private-key-file=$key_path \
-  #    --location=$GCP_REGION
-
-  gcloud certificate-manager certificates create $cert_name --location=$GCP_REGION --private-key-file=$key_path --certificate-file=$cert_path
+  gcloud certificate-manager certificates create $cert_name \
+     --certificate-file=$cert_path \
+     --private-key-file=$key_path \
+     --location=$GCP_REGION
 }
 
 
 # ---------------- Gateway security policy
 install_policies() {
   gcloud network-security gateway-security-policies import $policy_name \
-      --source=$policy_yaml \
+      --source=$RENDERED/policy.yaml \
       --location=$GCP_REGION
 }
 
 install_rules() {
-  gcloud network-security gateway-security-policies rules import allow-wikipedia-org \
-      --source=$rule_yaml \
-      --location=$GCP_REGION \
-      --gateway-security-policy=$policy_name
+    for rule_yaml in $RENDERED/rule-*.yaml; do
+        rule_name=$(basename "$rule_yaml" .yaml)
+        gcloud network-security gateway-security-policies rules import "$rule_name" \
+            --source="$rule_yaml" \
+            --location="$GCP_REGION" \
+            --gateway-security-policy="$policy_name"
+    done
 }
 
-
-# ---------------- THE PROXY ITSELF :party: --------------
+# ---------------- Secure Web Proxy
 install_proxy() {
-  gcloud network-services gateways import test-swp \
-      --source=$gateway_yaml \
+  gcloud network-services gateways import swp \
+      --source=$RENDERED/gateway.yaml \
       --location=$GCP_REGION
+}
+
+enable_apis() {
+  gcloud services enable networksecurity.googleapis.com
+  gcloud services enable certificatemanager.googleapis.com
+  gcloud services enable networkservices.googleapis.com
+}
+
+cleanup() {
+  rm $RENDERED/*
+
+  echo Deleting resources and disabling the APIs
+  gcloud certificate-manager certificates delete $cert_name --location=$GCP_REGION -q
+  gcloud network-services gateways delete swp --location=$GCP_REGION -q
+  gcloud network-security gateway-security-policies delete $policy_name --location=$GCP_REGION -q
+
+  sleep 250
+  gcloud services disable --force networksecurity.googleapis.com
+  gcloud services disable --force networkservices.googleapis.com
+  gcloud services disable --force certificatemanager.googleapis.com
 }
 
 main "$@"
 
-# ---------------- VPC and Subnets
 
+# ---------------- VPC and Subnets
 # proxy_subnet_name="${vpc_name}-proxy-only-subnet"
 # gcloud compute networks create $vpc_name --subnet-mode=custom
 # gcloud compute networks subnets create $subnet_name --range=$primary_range --network=$vpc_name --region=$GCP_REGION
--- gcp-secure-web-proxy/scripts/enable-apis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcloud services enable networksecurity.googleapis.com
-gcloud services enable certificatemanager.googleapis.com
-gcloud services enable networkservices.googleapis.com
