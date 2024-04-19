62657ddfd4d4dacff3617d6990df217a5b4f9a98
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2024-04-20 09:06:17 +1000

    tweaks

--- llm-on-gke/deploy.sh
+++ llm-on-gke/deploy.sh
@@ -9,5 +9,13 @@ kubectl create secret generic hf-secret \
     --from-literal=hf_api_token=$HF_TOKEN \
     --dry-run=client -o yaml | kubectl apply -f -
 
+kubectl apply -f "./k8s-manifests-gemma-2b-it-vllm.yaml"
+
+kubectl wait --for=condition=Available --timeout=700s deployment/vllm-gemma-deployment
+
+# kubectl logs -f -l app=gemma-server
+
+# kubectl port-forward service/llm-service 8000:8000
+
 # it - instruction tuned.
 # pretrained - shouldn't be deployed before it is tuned.
