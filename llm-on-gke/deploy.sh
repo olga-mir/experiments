4a5a54947ada29cd6f24748472504e6dbb993053
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2024-04-19 20:35:23 +1000

    add some stuff

--- llm-on-gke/deploy.sh
+++ llm-on-gke/deploy.sh
@@ -2,4 +2,12 @@
 
 set -eoux pipefail
 
+# https://cloud.google.com/kubernetes-engine/docs/tutorials/serve-gemma-gpu-vllm
 
+
+kubectl create secret generic hf-secret \
+    --from-literal=hf_api_token=$HF_TOKEN \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+# it - instruction tuned.
+# pretrained - shouldn't be deployed before it is tuned.
