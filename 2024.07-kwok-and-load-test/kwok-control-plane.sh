860a05cee6759dd338554aef6222ea090fa66a2e
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2024-07-28 14:36:34 +1000

    temp

---
+++ 2024.07-kwok-and-load-test/kwok-control-plane.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# https://kwok.sigs.k8s.io/docs/user/kwok-out-cluster/
+kwok \
+  --kubeconfig=~/.kube/config \
+  --manage-all-nodes=false \
+  --manage-nodes-with-annotation-selector=kwok.x-k8s.io/node=fake \
+  --manage-nodes-with-label-selector= \
+  --manage-single-node= \
+  --cidr=10.0.0.1/24 \
+  --node-ip=10.0.0.1 \
+  --node-lease-duration-seconds=40
+
+# create a node
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Node
+metadata:
+  annotations:
+    node.alpha.kubernetes.io/ttl: "0"
+    kwok.x-k8s.io/node: fake
+  labels:
+    beta.kubernetes.io/arch: amd64
+    beta.kubernetes.io/os: linux
+    kubernetes.io/arch: amd64
+    kubernetes.io/hostname: kwok-node-0
+    kubernetes.io/os: linux
+    kubernetes.io/role: agent
+    node-role.kubernetes.io/agent: ""
+    type: kwok
+  name: kwok-node-0
+spec:
+  taints: # Avoid scheduling actual running pods to fake Node
+  - effect: NoSchedule
+    key: kwok.x-k8s.io/node
+    value: fake
+status:
+  allocatable:
+    cpu: 32
+    memory: 256Gi
+    pods: 110
+  capacity:
+    cpu: 32
+    memory: 256Gi
+    pods: 110
+  nodeInfo:
+    architecture: amd64
+    bootID: ""
+    containerRuntimeVersion: ""
+    kernelVersion: ""
+    kubeProxyVersion: fake
+    kubeletVersion: fake
+    machineID: ""
+    operatingSystem: linux
+    osImage: ""
+    systemUUID: ""
+  phase: Running
+EOF
