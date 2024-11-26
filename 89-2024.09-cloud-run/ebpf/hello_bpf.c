97f07021b4b71f45bc4fce8ec4c8628436a58fd4
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2024-11-27 07:22:51 +1100

    misc updates to CR eBPF

--- 89-2024.09-cloud-run/ebpf/hello_bpf.c
+++ 89-2024.09-cloud-run/ebpf/hello_bpf.c
@@ -1,8 +1,10 @@
 #include <linux/bpf.h>
+#include <linux/ptrace.h>
+#include <linux/types.h>
 
 SEC("tracepoint/syscalls/sys_enter_execve")
-int hello_bpf(void *ctx) {
-    bpf_printk("Hello from eBPF on Cloud Run!\n");
+int hello_world(void *ctx) {
+    bpf_printk("Hello, eBPF World from Cloud Run!\n");
     return 0;
 }
 
