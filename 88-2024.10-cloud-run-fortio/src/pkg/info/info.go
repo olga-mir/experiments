4a6ab5ef3caf30955fe38483df29ae559992baea
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2024-12-30 15:44:04 +1100

    kernel check cross platform

--- 88-2024.10-cloud-run-fortio/src/pkg/info/info.go
+++ 88-2024.10-cloud-run-fortio/src/pkg/info/info.go
@@ -1,23 +1,114 @@
-package info
+package sysinfo
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/exec"
+	"reflect"
 	"runtime"
 	"strings"
+
+	"golang.org/x/sys/unix"
 )
 
+// Collector interface defines how to collect different types of information
+type Collector interface {
+	Collect() (map[string]interface{}, error)
+}
+
+// SystemInfo represents all collected information
+type SystemInfo struct {
+	collectors []Collector
+}
+
+// Individual collectors implement the Collector interface
+type UnixInfoCollector struct{}
+type NetworkInfoCollector struct{}
+type CloudRunCollector struct{}
+type MetadataCollector struct{}
+
 func HandleInfo(w http.ResponseWriter, r *http.Request) {
-	info := getSystemInfo()
+	sysInfo := sysinfo.NewSystemInfo(
+		&sysinfo.UnixInfoCollector{},
+		&sysinfo.NetworkInfoCollector{},
+		&sysinfo.CloudRunCollector{},
+		&sysinfo.MetadataCollector{},
+	)
+
+	// Collect all information
+	info, err := sysInfo.CollectAll()
+	if err != nil {
+		log.Fatalf("Error collecting system info: %v", err)
+	}
+
+	// Pretty print the results
+	output, _ := json.MarshalIndent(info, "", "  ")
+	fmt.Println(string(output))
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(info)
 }
 
+// UnixInfoCollector implementation
+func (c *UnixInfoCollector) Collect() (map[string]interface{}, error) {
+	u := unix.Utsname{}
+	if err := unix.Uname(&u); err != nil {
+		return nil, fmt.Errorf("uname error: %w", err)
+	}
+
+	return map[string]interface{}{
+		"os":      bytes.Trim(u.Sysname[:], "\x00"),
+		"release": bytes.Trim(u.Release[:], "\x00"),
+		"version": bytes.Trim(u.Version[:], "\x00"),
+		"machine": bytes.Trim(u.Machine[:], "\x00"),
+	}, nil
+}
+
+// NetworkInfoCollector collects information on network interfaces and any relevant network info
+func (c *NetworkInfoCollector) Collect() (map[string]interface{}, error) {
+	return map[string]interface{}{}, nil
+}
+
+// CloudRunCollector TBD
+func (c *CloudRunCollector) Collect() (map[string]interface{}, error) {
+	return map[string]interface{}{}, nil
+}
+
+// MetadataCollector print information retrieved from Metadata Server
+func (c *MetadataCollector) Collect() (map[string]interface{}, error) {
+	return map[string]interface{}{}, nil
+}
+
+// NewSystemInfo creates a new SystemInfo with specified collectors
+func NewSystemInfo(collectors ...Collector) *SystemInfo {
+	return &SystemInfo{
+		collectors: collectors,
+	}
+}
+
+// CollectAll gathers information from all collectors
+func (si *SystemInfo) CollectAll() (map[string]map[string]interface{}, error) {
+	results := make(map[string]map[string]interface{})
+
+	for _, collector := range si.collectors {
+		data, err := collector.Collect()
+		if err != nil {
+			return nil, fmt.Errorf("collection error: %w", err)
+		}
+
+		typeName := reflect.TypeOf(collector).Elem().Name()
+		results[typeName] = data
+	}
+
+	return results, nil
+}
+
+// / -------------------------------------
 type SystemInfo struct {
 	Hostname    string            `json:"hostname"`
 	Environment map[string]string `json:"environment"`
@@ -140,16 +231,10 @@ func getNetworkInfo() NetworkInfo {
 }
 
 func getSystemDetails() SystemDetails {
-	unameString := ""
-	if out, err := exec.Command("uname", "-a").Output(); err == nil {
-		unameString = strings.TrimSpace(string(out))
-	}
-
 	return SystemDetails{
 		OS:           runtime.GOOS,
 		Architecture: runtime.GOARCH,
 		NumCPU:       runtime.NumCPU(),
-		UnameInfo:    unameString,
 		GOMAXPROCS:   runtime.GOMAXPROCS(0),
 	}
 }
---
+++ 89-2024.09-ebpf/README.md
@@ -0,0 +1,12 @@
+# About
+
+Tinkering with eBPF (which is not acronym anymore and not a packet filtering) in cloud.
+
+# GKE
+
+WIP
+
+# Cloud Run
+
+One can hope...
+https://issuetracker.google.com/issues/206477810?pli=1
